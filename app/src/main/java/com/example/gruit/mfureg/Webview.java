9d75b52b3bec4223a5b9162efc17c7e8d2401bce
6131305032 <44702652+6131305032@users.noreply.github.com>
2018-11-29 13:10:04 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/gruit/mfureg/Webview.java
@@ -0,0 +1,24 @@
+package com.example.gruit.mfureg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Webview extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        String url = getIntent().getStringExtra("url");
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl(url);
+
+    }
+}
