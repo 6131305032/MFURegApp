8e8a7ce782c6f8a4f25a19532104a98672c16f5e
gruit <6131305032@lamduan.mfu.ac.th>
2018-11-29 23:05:57 +0700

    Update Nov29

--- app/src/main/java/com/example/gruit/mfureg/ScrollingActivity.java
+++ app/src/main/java/com/example/gruit/mfureg/ScrollingActivity.java
@@ -1,71 +1,46 @@
 package com.example.gruit.mfureg;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 
-public class ScrollingActivity extends AppCompatActivity {
+//make your class implement OnClickListener
+public class ScrollingActivity extends AppCompatActivity implements OnClickListener {
 
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
 
-        findViewById(R.id.bTimetable).setOnClickListener(new View.OnClickListener()
+        //Create your buttons and set their onClickListener to "this"
+
+        LinearLayout bTimetable = (LinearLayout) findViewById(R.id.bTimetable);
+        bTimetable.setOnClickListener(this);
+
+        LinearLayout bBio = (LinearLayout) findViewById(R.id.bBio);
+        bBio.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(ScrollingActivity.this, Webview.class);
+        switch(v.getId())
         {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(ScrollingActivity.this, Webview.class);
+            case R.id.bTimetable:
+                intent.putExtra("url", "https://www.google.com");
+                startActivity(intent);
+                break;
 
+            case R.id.bBio:
+                intent.putExtra("url", "https://www.google.com");
+                startActivity(intent);
+                break;
 
-                switch(v.getId())
-                {
-                    case R.id.bTimetable:
-                        intent.putExtra("url", "https://docs.google.com/spreadsheets/d/1bKWpQPVEZs3t33dbksIhZ0d81g_RtHV4IVixLhmIXJk/");
-                        startActivity(intent);
-                        break;
-
-                    default:
-                        break;
-                }
-
-            }
-
-        });
-
-        findViewById(R.id.bBio).setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(ScrollingActivity.this, Webview.class);
-
-
-                switch(v.getId())
-                {
-                    case R.id.bBio:
-                        intent.putExtra("url", "http://wws.princeton.edu/");
-                        startActivity(intent);
-                        break;
-
-                    default:
-                        break;
-                }
-
-            }
-
-        });
+            default:
+                break;
+        }
     }
 }
